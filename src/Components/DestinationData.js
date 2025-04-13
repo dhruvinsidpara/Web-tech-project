500915915396ca548407e13ca27fc1950cc1c74a
Valakeval001 <keval50582@gmail.com>
2025-04-13 23:09:51 +0530

    DestinationData store all Destination point

--- src/Components/DestinationData.js
+++ src/Components/DestinationData.js
@@ -1,1 +1,21 @@
 
+import { Component } from "react";
+import mubai from "../assets/mubai.jpg"
+import "./Destination.css"
+
+class DestinationData extends Component{
+    render(){
+        return (
+            <div className={this.props.className}>
+            <div className="des-text">
+                <h2> {this.props.heading}</h2>
+                <p> {this.props.text}</p>
+            </div>
+            <div className="image">
+                <img alt="img" src={this.props.img1}/>
+            </div>
+         </div>
+        );
+    }
+}
+export default DestinationData
