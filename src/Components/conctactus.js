042f104e23226330e8c595c459a2f7ca5ffba38a
PritamHirani <pritamhirani2005@gmail.com>
2025-04-14 19:45:28 +0530

    Update conctactus.js

--- src/Components/conctactus.js
+++ src/Components/conctactus.js
@@ -1,1 +1,20 @@
+import "./ContactForm.css";
+function ContactForm(){
+    return (
+        <div className="from-container">
+            <h1>
+                send a message to us!.
+            </h1>
+            <form>
+                <input placeholder="Name"/>  
+                <input placeholder="Email"/>    
+                <input placeholder="Subject"/>    
+                <textarea placeholder="message" rows="4"></textarea>
+                <button> send massge </button>
+           </form>
+  
+         </div>
 
+    );
+}
+export default ContactForm;
