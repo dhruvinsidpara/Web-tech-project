bc57b7a9a7641116973eac2b00f49a5bbe65e05d
dhruvinsidpara <dhruvinsidpara887@gmail.com>
2025-04-15 19:39:23 +0530

    Update About.js

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,23 +1,25 @@
 import Navbar from "../component/Navbar"
 import Hero from "../component/Hero"
 import AboutImg from "../assets/11.jpeg"
+import Footer from "../component/Footer";
+import Aboutus from "../component/Aboutus";
 function About (){
     return(
         <>
          <Navbar />
       
-
         <Hero
          cName="hero-mid"
          heroImg={AboutImg}
          title="About"
          
          />
+         <Aboutus/>
+
+        <Footer/>
           
         </>
     );
 
 }
 export default About;
-
-
